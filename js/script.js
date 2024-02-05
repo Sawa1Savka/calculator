ea57e33947080d81ea45c8fe72e5e2ab26b66885
Sawa1Savka <133581993+Sawa1Savka@users.noreply.github.com>
2024-02-05 09:47:23 +0300

    Calculator

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿function appendNumber(number) {
+  document.getElementById("result").value += number;
+}
+
+function appendOperator(operator) {
+  document.getElementById("result").value += operator;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculateResult() {
+  let expression = document.getElementById("result").value;
+  // Заменяем символ % на /100
+  expression = expression.replace(/%/g, "/100");
+  // Вычисляем корень
+  expression = expression.replace(/√(\d+)/g, "Math.sqrt($1)");
+  const result = eval(expression);
+  document.getElementById("result").value = result;
+}
+
+function handleKeyPress(event) {
+  if (event.key === "Enter") {
+    calculateResult();
+  }
+}
